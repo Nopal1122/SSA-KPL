947048348f354f5a80fbaba5ce642851ba02f6c4
Naufal <ahmadnaufalalghifari@gmail.com>
2025-05-01 13:53:36 +0700

    revisi

--- Kpl_tubes/Model/User.cs
+++ Kpl_tubes/Model/User.cs
@@ -9,22 +9,6 @@ namespace Kpl_tubes.Model
 
     public abstract  class User
     {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string Email { get; set; }
-        public string Password { get; set; }
-        public string PhoneNumber { get; set; }
-        public string Address { get; set; }
-        public bool IsVerified { get; set; }
-        // Constructor
-        protected User(string name, string email, string password, string phoneNumber, string address)
-        {
-            Name = name;
-            Email = email;
-            Password = password;
-            PhoneNumber = phoneNumber;
-            Address = address;
-            IsVerified = false;
-        }
+     
     }
 }
